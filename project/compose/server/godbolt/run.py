ad37ed0fde153e756319cbb57cbc9e4cf273435e
AnastaZIuk <areklachowicz@gmail.com>
2024-01-17 04:06:18 +0100

    Implement healthy checks for clients in order to godbolt proxy insstance to wait a proper amount of time before sending requests, update compose and run module files

--- project/compose/server/godbolt/run.py
+++ project/compose/server/godbolt/run.py
@@ -1,14 +1,50 @@
-import os, subprocess, sys
+import os, subprocess, sys, argparse, socket
+
+def getLocalIPV4():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ipv4 = s.getsockname()[0] # Get the local IPv4 address
+        s.close()
+        
+        return ipv4
+    except socket.error:
+        return None
+    
+def healthyCheck():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(5)
+            s.connect((getLocalIPV4(), 10240))
+        
+        print(f"Connected to localhost godbolt client instance, healthy check passed")
+
+        return True
+    except (socket.error, socket.timeout):
+        print(f"Excpetion caught while trying to connect to localhost godbolt client instance, healthy check didnt pass: \"{socket.error}\"")
+        return False
 
 try:
-    GIT_GODBOLT_REPOSITORY_PATH = os.environ.get('GIT_GODBOLT_REPOSITORY_PATH', '')
+    parser = argparse.ArgumentParser(description="server.godbolt.run Framework Module")
     
-    cmd = [
-        "npm", "run", "dev", "--", "--language", "c++,hlsl"
-    ]
+    parser.add_argument("--healthyCheck", help="Check if service is healthy and server is running", action="store_true")
 
-    os.chdir(GIT_GODBOLT_REPOSITORY_PATH)
-    subprocess.run(cmd, shell=True)
+    args = parser.parse_args()
+
+    if args.healthyCheck:
+        if healthyCheck():
+            sys.exit(0)  # healthy
+        else:
+            sys.exit(-1)
+    else:
+        GIT_GODBOLT_REPOSITORY_PATH = os.environ.get('GIT_GODBOLT_REPOSITORY_PATH', '')
+        
+        cmd = [
+            "npm", "run", "dev", "--", "--language", "c++,hlsl"
+        ]
+
+        os.chdir(GIT_GODBOLT_REPOSITORY_PATH)
+        subprocess.run(cmd, shell=True)
          
 except subprocess.CalledProcessError as e:
     print(f"Subprocess failed with exit code {e.returncode}")
