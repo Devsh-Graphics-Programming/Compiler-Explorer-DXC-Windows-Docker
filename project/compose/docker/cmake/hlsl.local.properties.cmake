13d40bc4095900ac0622b39f688314acc5f4e844
AnastaZIuk <areklachowicz@gmail.com>
2024-01-26 15:44:11 +0100

    Add .notification with an URL to DXC commit handled by containers

--- project/compose/docker/cmake/hlsl.local.properties.cmake
+++ project/compose/docker/cmake/hlsl.local.properties.cmake
@@ -35,9 +35,24 @@ group.dxc.groupName=DXC compilers
 
 compiler.dxc_upstream.exe=@DXC_EXECUTABLE@
 compiler.dxc_upstream.name=DXC
+compiler.dxc_upstream.notification=The DXC has been compiled from following <a href="https://github.com/microsoft/DirectXShaderCompiler/commit/@DXC_SHA@" target="_blank" rel="noopener noreferrer">commit<sup><small class="fas fa-external-link-alt opens-new-window" title="Opens the commit in a new window"></small></sup></a>.
+
 ]=]
 )
 	message(STATUS "DXC_EXECUTABLE = \"${DXC_EXECUTABLE}\"")
+
+	execute_process(COMMAND git -C "$ENV{GIT_DXC_REPOSITORY_PATH}" rev-parse HEAD
+		RESULT_VARIABLE _RESULT
+		OUTPUT_VARIABLE DXC_SHA
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+
+	if("${_RESULT}" STREQUAL "0")
+		message(STATUS "DXC_SHA = \"${DXC_SHA}\"")
+	else()
+		message(FATAL_ERROR "Could not parse local HEAD SHA of \"$ENV{GIT_DXC_REPOSITORY_PATH}\" repository!")
+	endif()
+
 endif()
 
 message(STATUS "Creating \"${OUTPUT_HLP_PATH}\"")
