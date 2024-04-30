f8d00b210f45493792c43a08853b23b67b5b58de
AnastaZIuk <areklachowicz@gmail.com>
2024-04-30 16:51:04 +0200

    Introduce multi-stage builds to Nabla client, override CMake version against VS' ones

--- project/compose/client/nabla/docker/cmake/hlsl.local.properties.cmake
+++ project/compose/client/nabla/docker/cmake/hlsl.local.properties.cmake
@@ -2,6 +2,10 @@ if(NOT DEFINED OUTPUT_HLP_PATH)
 	message(FATAL_ERROR "OUTPUT_HLP_PATH must be defined!")
 endif()
 
+if(NOT DEFINED NABLA_REV_TARGET)
+	message(FATAL_ERROR "NABLA_REV_TARGET must be defined!")
+endif()
+
 string(APPEND IMPL_CONTENT
 [=[
 compilers=&dxc
@@ -22,7 +26,7 @@ group.dxc.groupName=NSC compilers
 
 compiler.nsc_release_upstream.exe=@NSC_RELEASE_EXECUTABLE@
 compiler.nsc_release_upstream.name=NSC (Release)
-compiler.nsc_release_upstream.notification=The NSC (Release) has been compiled from following <a href="https://github.com/Devsh-Graphics-Programming/Nabla/commit/@NABLA_SHA@" target="_blank" rel="noopener noreferrer">commit<sup><small class="fas fa-external-link-alt opens-new-window" title="Opens the commit in a new window"></small></sup></a>.
+compiler.nsc_release_upstream.notification=The NSC (Release) has been compiled from following <a href="https://github.com/Devsh-Graphics-Programming/Nabla/commit/@NABLA_REV_TARGET@" target="_blank" rel="noopener noreferrer">commit<sup><small class="fas fa-external-link-alt opens-new-window" title="Opens the commit in a new window"></small></sup></a>.
 compiler.nsc_release_upstream.supportsExecute=false
 compiler.nsc_release_upstream.options=
 compiler.nsc_release_upstream.disassemblerPath=@SPIRV_DIS_EXE@
@@ -30,7 +34,7 @@ compiler.nsc_release_upstream.demangler=
 
 compiler.nsc_debug_upstream.exe=@NSC_DEBUG_EXECUTABLE@
 compiler.nsc_debug_upstream.name=NSC (Debug)
-compiler.nsc_debug_upstream.notification=The NSC (Debug) has been compiled from following <a href="https://github.com/Devsh-Graphics-Programming/Nabla/commit/@NABLA_SHA@" target="_blank" rel="noopener noreferrer">commit<sup><small class="fas fa-external-link-alt opens-new-window" title="Opens the commit in a new window"></small></sup></a>.
+compiler.nsc_debug_upstream.notification=The NSC (Debug) has been compiled from following <a href="https://github.com/Devsh-Graphics-Programming/Nabla/commit/@NABLA_REV_TARGET@" target="_blank" rel="noopener noreferrer">commit<sup><small class="fas fa-external-link-alt opens-new-window" title="Opens the commit in a new window"></small></sup></a>.
 compiler.nsc_debug_upstream.supportsExecute=false
 compiler.nsc_debug_upstream.options=
 compiler.nsc_debug_upstream.disassemblerPath=@SPIRV_DIS_EXE@
@@ -38,34 +42,20 @@ compiler.nsc_debug_upstream.demangler=
 ]=]
 )
 
-set(NSC_BIN_DIRECTORY "$ENV{GIT_NABLA_DIRECTORY}/tools/nsc/bin")
-
 find_program(NSC_RELEASE_EXECUTABLE
 	NAMES nsc
-	HINTS "${NSC_BIN_DIRECTORY}"
+	HINTS "$ENV{GIT_NABLA_DIRECTORY}/install/exe/tools/nsc/bin"
 	REQUIRED
 )
 
 find_program(NSC_DEBUG_EXECUTABLE
 	NAMES nsc_d
-	HINTS "${NSC_BIN_DIRECTORY}"
+	HINTS "$ENV{GIT_NABLA_DIRECTORY}/install/debug/exe/tools/nsc/bin"
 	REQUIRED
 )
 
 message(STATUS "NSC_RELEASE_EXECUTABLE = \"${NSC_RELEASE_EXECUTABLE}\"")
 message(STATUS "NSC_DEBUG_EXECUTABLE = \"${NSC_DEBUG_EXECUTABLE}\"")
-
-execute_process(COMMAND git -C "$ENV{GIT_NABLA_DIRECTORY}" rev-parse HEAD
-	RESULT_VARIABLE _RESULT
-	OUTPUT_VARIABLE NABLA_SHA
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-if("${_RESULT}" STREQUAL "0")
-	message(STATUS "NABLA_SHA = \"${NABLA_SHA}\"")
-else()
-	message(FATAL_ERROR "Could not parse local HEAD SHA of \"$ENV{GIT_NABLA_DIRECTORY}\" repository!")
-endif()
 
 find_program(SPIRV_DIS_EXE
 	NAMES spirv-dis
